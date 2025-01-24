0f5e6696317864f05a3e428a472631b5ddcfe24e
TheLvableMrFish <paganochristians@gmail.com>
2025-01-23 21:55:10 -0600

    Fixed a rounding issue with the localstorage amount

--- src/Apps/Folder.js
+++ src/Apps/Folder.js
@@ -21,7 +21,7 @@ const Folder =({})=>{
         const sizes = ['Bytes', 'KB', 'MB']
         if(bytes ===0) return '0 Byte'
         const i = Math.floor(Math.log(bytes) / Math.log(1024))
-        return (bytes/Math.pow(1024, i).toFixed(2) + ' ' + sizes[i])
+        return ((bytes/Math.pow(1024, i)).toFixed(2) + ' ' + sizes[i])
     }
 
     const calculateStorageUsage =()=>{

5d92b72b1cc69734af9d43ab69c9c716a69e31da
TheLvableMrFish <paganochristians@gmail.com>
2025-02-18 21:07:55 -0600

    Edited the css for my_projects_css.css so that my projects can be scrolled through and added links

--- src/Apps/MyProjects.js
+++ src/Apps/MyProjects.js
@@ -9,15 +9,26 @@ const MyProjects =({})=>{
 
     return(
         <>
-        <div className='about-me-box container-fluid'>
+        <div className='about-me-box my-project-scrollable-container container-fluid'>
             <div className='row'>
                 {my_project_data.map((project, index)=>(
                     <div className='my-project-item col-6'>
                         <h6 className='my-project-title'>{project.title}</h6>
                         <img className='my-project-img img-fluid' src={`./projects/${project.img}.png`} />
                         <div className='my-project-btns row'>
-                        <a className='my-project-code my-project-btn' href=''>Code</a>
-                        <a className='my-project-live my-project-btn' href=''>Live</a>
+
+                        {}
+                        <a 
+                            className={`my-project-code my-project-btn ${project.code_link === 'n/a' ? 'my-project-not-active' : ''}`} 
+                            target='_blank'
+                            href={project.code_link != 'n/a' ? project.code_link : '#'}
+                        >Code</a>
+                        <a 
+                            className={`my-project-live my-project-btn ${project.host_link === 'n/a' ? 'my-project-not-active' : ''}`} 
+                            target='_blank'
+                            href={project.host_link != 'n/a' ? project.host_link : '#'}
+                        >Live</a>
+
                         </div>
                     </div>
                 ))

9b6de04c7231ab216b21ed40d83ea342adf3fca9
TheLvableMrFish <paganochristians@gmail.com>
2025-02-24 20:47:46 -0600

    Added a temp grid outline to show where how I want my applications to align onto the device, may change

--- src/Pages/Mobile.js
+++ src/Pages/Mobile.js
@@ -5,12 +5,70 @@ const Mobile =()=>{
     return(
         <>
         <div className='container-fluid'>
-            <div className='header row'>
-                <div className='col'>6:40</div>
-                <div className='col'>icon</div>
-                <div className='col'><img src='./mobileMedia/wifi.png' /></div>
-                <div className='col'><img src='./mobileMedia/5G.png' /></div>
-                <div className='col'><img src='./mobileMedia/battery-mid.png' /></div>
+            <div className='mobile-header header row'>
+                <div className='mobile-header-item col'>6:40</div>
+                <div className='mobile-header-item col-auto'>
+                    <img className='mobile-icon' src='./mobileMedia/wifi.png' />
+                    <img className='mobile-icon' src='./mobileMedia/5G.png' />
+                    <img className='mobile-icon' src='./mobileMedia/battery-mid.png' />
+                </div>
+            </div>
+
+            <div className='mobile-grid row'>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+            </div>
+            <div className='mobile-grid row'>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+            </div>
+            <div className='mobile-grid row'>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+            </div>
+            <div className='mobile-grid row'>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+            </div>
+            <div className='mobile-grid row'>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+            </div>
+            <div className='mobile-grid row'>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+            </div>
+            <div className='mobile-grid row'>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
+                <div className='mobile-grid-space col-2'>x</div>
             </div>
 
             <div className='mobile-footer footer row'>
